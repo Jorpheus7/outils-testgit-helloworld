f64f1fb8e84fe2781658bab8b751ed691adf11d7
Marion <marion@bechennec.name>
2014-04-01 17:04:21 +0200

    HELLO-1 javadoc added to method "getMessage" from HelloWorld class

--- src/java/com/emn/helloworld/modele/HelloWorld.java
+++ src/java/com/emn/helloworld/modele/HelloWorld.java
@@ -2,6 +2,10 @@ package java.com.emn.helloworld.modele;
 
 public class HelloWorld {
 	
+	/**
+	 * 
+	 * @return a String message which says "Hello World !"
+	 */
 	public String getMessage(){
 		return "Hello World !";
 	}
